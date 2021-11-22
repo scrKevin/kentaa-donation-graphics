dce6ba9057c318801665ab832c3aca375615f656
scrKevin <scrKevin@github.com>
2021-11-22 15:32:22 +0100

    add https and stopwatch

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+const fs = require('fs')
+
+const httpsCheck = false
+
+const keyPath = __dirname + '/cert/privkey.pem'
+const certificatePath = __dirname + '/cert/fullchain.pem'
+
+try {
+  if (fs.existsSync(keyPath) && fs.existsSync(certificatePath)) {
+    httpsCheck = true
+  }
+} catch(err) {
+  console.log("no https")
+}
+
 var http = require('http');
+
+if (httpsCheck) {
+  var https = require('https')
+  var privateKey  = fs.readFileSync(__dirname + '/cert/privkey.pem', 'utf8');
+  var certificate = fs.readFileSync(__dirname + '/cert/fullchain.pem', 'utf8');
+  var credentials = {key: privateKey, cert: certificate};
+}
 
 var express = require('express');
 var app = express();
@@ -41,13 +63,30 @@ app.get('/amount/:type/:id', function(req, res) {
   res.render('pages/amount/index');
 });
 
-app.get('/twitch', function(req, res) {
-  res.render('pages/twitch/index');
+// stopwatch page
+app.get('/stopwatch/:year/:month/:day/:hour/:minute/:second', function(req, res) {
+  var startDate = {
+    year: req.params['year'],
+    month: req.params['month'],
+    day: req.params['day'],
+    hour: req.params['hour'],
+    minute: req.params['minute'],
+    second: req.params['second']
+  }
+  res.render('pages/stopwatch/index', {startDate: startDate});
+});
+
+// /favicon.ico
+app.get('/favicon.ico', function(req, res) {
+  res.sendStatus(404)
+});
+
+app.get('/:page', function(req, res) {
+  res.render('pages/' + req.params['page'] + '/index');
 });
 
 var httpServer = http.createServer(app);
 httpServer.listen(8080);
-//app.listen(8080);
 
 httpServer.on('upgrade', function upgrade(request, socket, head) {
   console.log(request.url)
@@ -73,4 +112,37 @@ httpServer.on('upgrade', function upgrade(request, socket, head) {
     }
   }
 });
-console.log('Server is listening on port 8080');
+
+console.log('Http Server is listening on port 8080');
+
+if (httpsCheck) {
+  var httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(443);
+
+  httpsServer.on('upgrade', function upgrade(request, socket, head) {
+    console.log(request.url)
+    var urlSegments = request.url.split("/")
+    if (urlSegments.length == 4) {
+      if (urlSegments[1] === 'donations') {
+        for (let donationTracker of donationTrackers) {
+          if (donationTracker.type == urlSegments[2] && donationTracker.id == urlSegments[3]) {
+            donationTracker.wss.handleUpgrade(request, socket, head, function done(ws) {
+              donationTracker.wss.emit('connection', ws, request);
+            });
+          }
+        }
+      }
+      else if (urlSegments[1] === 'amount') {
+        for (let amountTracker of amountTrackers) {
+          if (amountTracker.type == urlSegments[2] && amountTracker.id == urlSegments[3]) {
+            amountTracker.wss.handleUpgrade(request, socket, head, function done(ws) {
+              amountTracker.wss.emit('connection', ws, request);
+            });
+          }
+        }
+      }
+    }
+  });
+  console.log('Https Server is listening on port 443');
+}
+
